86baf03a1037e224eba3ee2a02b78a57501d5bdf
Tanawat Tassana <amp.tanawat@gmail.com>
2018-07-28 03:20:59 +0700

    Generate action files

--- libs/createAction.js
+++ libs/createAction.js
@@ -9,4 +9,21 @@ module.exports = function(generator, stateName, actionName) {
     code: `${stateName}/${_.toUpper(_.snakeCase(actionName))}`,
   })
   generator.updateMeta()
+
+  generator.fs.copyTpl(
+    templatePath('actions/index.ejs'),
+    generator.destinationPath(`src/state/${stateName}/actions/index.js`),
+    {
+      state: stateName,
+      actions: meta.states[stateName].actions,
+    }
+  )
+
+  generator.fs.copyTpl(
+    templatePath('actions/action.ejs'),
+    generator.destinationPath(`src/state/${stateName}/actions/${actionName}.js`),
+    {
+      action: meta.states[stateName].actions[actionName],
+    }
+  )
 }
