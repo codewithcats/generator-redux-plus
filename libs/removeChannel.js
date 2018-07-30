43edf474361349e95793c9f8d73f00f7825a1be6
Veckenz <veckenz@gmail.com>
2018-07-30 15:29:19 +0700

    change check channel reducer and meta variable

--- libs/removeChannel.js
+++ libs/removeChannel.js
@@ -2,10 +2,8 @@ const _ = require('lodash')
 const templatePath = require('./templatePath')
 
 module.exports = function(generator, stateName, channelsName) {
-  const meta = generator.meta
-
   channelsName.forEach(channel => {
-    _.unset(meta, `states.${stateName}.channels.${channel}`)
+    _.unset(generator.meta, `states.${stateName}.channels.${channel}`)
   })
 
   generator.updateMeta()
