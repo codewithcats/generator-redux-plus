223085a004988ff7c14316623ec87cec98845b37
Tanawat Tassana <amp.tanawat@gmail.com>
2018-07-30 10:55:07 +0700

    Create reducer from action data

--- libs/createReducer.js
+++ libs/createReducer.js
@@ -1,6 +1,20 @@
 const _ = require('lodash')
 const templatePath = require('./templatePath')
 
-module.exports = function(generator, stateName, reducerName) {
-  console.log('create reducer', stateName, reducerName)
+module.exports = function(generator, stateName, actionName) {
+  const action = generator.getActions(stateName)[actionName]
+  action.reducer = true
+  generator.updateMeta()
+
+  const state = generator.getState(stateName)
+  const reducer = generator.getReducers(stateName)[actionName]
+
+  generator.fs.copyTpl(
+    templatePath('reducers/reducer.ejs'),
+    generator.destinationPath(`src/state/${stateName}/reducers/${actionName}.js`),
+    {
+      state,
+      action,
+    }
+  )
 }
